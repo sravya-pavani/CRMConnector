b73c53d4ff1b4e3a27ac6881f9866b83561b23f8
Pavani Amanchi <79402303+sravya-pavani@users.noreply.github.com>
2022-02-15 18:37:05 +0100

    Initial Working solution

---
+++ CrmServiceConnection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Net;
+
+namespace Arbetsprov
+{
+    class CrmServiceConnection
+    {
+        /// <summary>
+        /// Connect to CRM based on Client ID and Client Secret retrieved from Azure AD
+        /// </summary>
+        /// <returns></returns>
+        public static void GetOrganizationServiceClientSecret()
+        {
+            //Gets all the config values
+            string url = SiteGlobal.CRMUrl;
+            string clientId = SiteGlobal.ClientID;
+            string clientSecret = SiteGlobal.ClientSecret;
+
+            //Connect to CRM using CrmServiceClient with CLient ID and Client Secret
+            //and returns the OrganizationService object.
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                var conn = new CrmServiceClient($@"AuthType=ClientSecret;url={url};ClientId={clientId};ClientSecret={clientSecret}");
+
+                SiteGlobal.OrgService = conn.OrganizationWebProxyClient != null ? conn.OrganizationWebProxyClient : (IOrganizationService)conn.OrganizationServiceProxy;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while connecting to CRM " + ex.Message);
+                Console.ReadKey();
+                return null;
+            }          
+        }
+    }
+}
---
+++ SiteGlobal.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Configuration;
+
+namespace Arbetsprov
+{
+    internal class SiteGlobal
+    {
+        /// <summary>
+        /// Gets or sets the CRM Url.
+        /// </summary>
+        /// <value>CRM Instance Url.</value>
+        static public string CRMUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the client id.
+        /// </summary>
+        /// <value>Client ID.</value>
+        static public string ClientID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the client secret.
+        /// </summary>
+        /// <value>The content type header.</value>
+        static public string ClientSecret { get; set; }        
+
+        /// <summary>
+        /// Gets or sets the entity name in which the records need to be inserted.
+        /// </summary>
+        static public string CreateRecordInEntity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the organization service value.
+        /// </summary>
+        static public IOrganizationService OrgService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name of a record in contact entity.
+        /// </summary>
+        static public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name of a record in contact entity.
+        /// </summary>
+        static public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address of a record in contact entity.
+        /// </summary>
+        static public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address line of a record in contact entity.
+        /// </summary>
+        static public string AddressLine1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the file location.
+        /// </summary>
+        static public string FileLocation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the delimiter in the CSV file.
+        /// </summary>
+        static public string Delimiter { get; set; }
+        static SiteGlobal()
+        {
+
+            CRMUrl = ConfigurationManager.AppSettings["crmUrl"];
+            ClientID = ConfigurationManager.AppSettings["clientID"];
+            ClientSecret = ConfigurationManager.AppSettings["clientSecret"];
+            CreateRecordInEntity = ConfigurationManager.AppSettings["createRecordInEntity"];
+            FirstName = ConfigurationManager.AppSettings["firstname"];
+            LastName = ConfigurationManager.AppSettings["lastname"];
+            EmailAddress = ConfigurationManager.AppSettings["emailaddress1"];
+            AddressLine1 = ConfigurationManager.AppSettings["address1_line1"];
+            FileLocation = ConfigurationManager.AppSettings["fileLocation"];
+            Delimiter = ConfigurationManager.AppSettings["delimiter"];
+        }
+    }
+}
